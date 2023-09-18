93fd40eec3257897e5d15b88b2e11d715b97dede
105gun <ooo693428@163.com>
2023-09-19 02:55:11 +0800

    0.0.1 A simple implementation of spaceship speed control.

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -15,6 +15,59 @@ DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
 	return data;
 }();
 
+std::string n2hexstr(int w, size_t hex_len = sizeof(int)<<1) {
+    static const char* digits = "0123456789ABCDEF";
+    std::string rc(hex_len,'0');
+    for (size_t i=0, j=(hex_len-1)*4 ; i<hex_len; ++i,j-=4)
+        rc[i] = digits[(w>>j) & 0x0f];
+    return rc;
+}
+
+static REL::Relocation<__int64 (*)(double, char*, ...)> ExecuteCommand{ REL::Offset(0x287DF04) };
+int crtShip = 0;
+
+static DWORD MainLoop(void* unused)
+{
+    (void)unused;
+    for (;;)
+    {
+		int up = SFSE::WinAPI::GetKeyState(33);
+		int down = SFSE::WinAPI::GetKeyState(34);
+		auto* player = RE::Actor::PlayerCharacter();
+		if (player){
+			RE::TESObjectREFR* ship = player->GetAttachedSpaceship();
+			if (ship){
+				SFSE::log::info("CRT: {} {}", n2hexstr(ship->formID), std::to_string(down));
+				crtShip = ship->formID;
+			}
+			else {
+				crtShip = 0;
+			}
+
+		}
+		if(crtShip) {
+			std::string prefix = n2hexstr(crtShip);
+			if (up < 0) {
+				char* string1 = (prefix + std::string(".setav spaceshipenginepartmaxforwardspeed 10000")).data();
+				char* string2 = (prefix + std::string(".setav spaceshipforwardspeedmult 10000")).data();
+				char* string3 = (prefix + std::string(".setav spaceshipboostspeed 1500")).data();
+				ExecuteCommand(0, string1);
+				ExecuteCommand(0, string2);
+				ExecuteCommand(0, string3);
+			} else if (down < 0) {
+				char* string1 = (prefix + std::string(".setav spaceshipenginepartmaxforwardspeed 300")).data();
+				char* string2 = (prefix + std::string(".setav spaceshipforwardspeedmult 300")).data();
+				char* string3 = (prefix + std::string(".setav spaceshipboostspeed 4")).data();
+				ExecuteCommand(0, string1);
+				ExecuteCommand(0, string2);
+				ExecuteCommand(0, string3);
+			}
+		}
+        Sleep(100);
+    }
+    return 0;
+}
+
 namespace
 {
 	void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept
@@ -22,6 +75,7 @@ namespace
 		switch (a_msg->type) {
 		case SFSE::MessagingInterface::kPostLoad:
 			{
+				CreateThread(NULL, 4096, MainLoop, NULL, 0, NULL);
 				break;
 			}
 		default:
