8aa14a869e7505378adec920585c01d6d657e58c
105gun <ooo693428@163.com>
2023-09-19 19:59:25 +0800

    work in progress...

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -24,7 +24,63 @@ std::string n2hexstr(int w, size_t hex_len = sizeof(int)<<1) {
 }
 
 static REL::Relocation<__int64 (*)(double, char*, ...)> ExecuteCommand{ REL::Offset(0x287DF04) };
-int crtShip = 0;
+auto* LOGGER = RE::ConsoleLog::GetSingleton();
+// int StateMachine.GetInstance().iCurrentShip = 0;
+
+class State
+{
+	int iMaxForwardSpeed;
+	int iForwardSpeedMult;
+	int iBoostSpeed;
+	State(int mfs, int fsm, int bs) {
+		iMaxForwardSpeed = mfs;
+		iForwardSpeedMult = fsm;
+		iBoostSpeed = bs;
+	}
+
+	void Enter() {
+
+	}
+
+	void Exit() {
+
+	}
+}
+
+class StateMachine
+{
+	enum class InputType{
+		
+	}
+
+	static StateMachine& GetInstance() {
+		static StateMachine singleton;
+		return singleton;
+	}
+
+	int iCurrentShip;
+	std::vector<State> vStateList;
+
+	StateMachine() {
+		vStateList.push_back(new State(300, 300, 4));
+		vStateList.push_back(new State(10000, 10000, 1500));
+		vStateList.push_back(new State(100000, 100000, 15000));
+		vStateList.push_back(new State(30000000, 1000000, 150000));
+	}
+
+	void ChangeStateMachine() {
+
+	}
+
+
+}
+
+
+void SetActorValue(int formID, std::string name, int value)
+{
+	std::string result = fmt::format("{}.setav {} {}", n2hexstr(formID), name, value);
+	ExecuteCommand(0, result.data());
+}
 
 static DWORD MainLoop(void* unused)
 {
@@ -38,29 +94,35 @@ static DWORD MainLoop(void* unused)
 			RE::TESObjectREFR* ship = player->GetAttachedSpaceship();
 			if (ship){
 				SFSE::log::info("CRT: {} {}", n2hexstr(ship->formID), std::to_string(down));
-				crtShip = ship->formID;
+				StateMachine.GetInstance().iCurrentShip = ship->formID;
 			}
 			else {
-				crtShip = 0;
+				StateMachine.GetInstance().iCurrentShip = 0;
 			}
 
 		}
-		if(crtShip) {
-			std::string prefix = n2hexstr(crtShip);
+		if(StateMachine.GetInstance().iCurrentShip) {
+			//std::string prefix = n2hexstr(StateMachine.GetInstance().iCurrentShip);
 			if (up < 0) {
-				char* string1 = (prefix + std::string(".setav spaceshipenginepartmaxforwardspeed 10000")).data();
+				SetActorValue(StateMachine.GetInstance().iCurrentShip, "spaceshipenginepartmaxforwardspeed", 10000);
+				SetActorValue(StateMachine.GetInstance().iCurrentShip, "spaceshipforwardspeedmult", 10000);
+				SetActorValue(StateMachine.GetInstance().iCurrentShip, "spaceshipboostspeed", 1500);
+				/*char* string1 = (prefix + std::string(".setav spaceshipenginepartmaxforwardspeed 10000")).data();
 				char* string2 = (prefix + std::string(".setav spaceshipforwardspeedmult 10000")).data();
 				char* string3 = (prefix + std::string(".setav spaceshipboostspeed 1500")).data();
 				ExecuteCommand(0, string1);
 				ExecuteCommand(0, string2);
-				ExecuteCommand(0, string3);
+				ExecuteCommand(0, string3);*/
 			} else if (down < 0) {
-				char* string1 = (prefix + std::string(".setav spaceshipenginepartmaxforwardspeed 300")).data();
+				SetActorValue(StateMachine.GetInstance().iCurrentShip, "spaceshipenginepartmaxforwardspeed", 300);
+				SetActorValue(StateMachine.GetInstance().iCurrentShip, "spaceshipforwardspeedmult", 300);
+				SetActorValue(StateMachine.GetInstance().iCurrentShip, "spaceshipboostspeed", 4);
+				/*char* string1 = (prefix + std::string(".setav spaceshipenginepartmaxforwardspeed 300")).data();
 				char* string2 = (prefix + std::string(".setav spaceshipforwardspeedmult 300")).data();
 				char* string3 = (prefix + std::string(".setav spaceshipboostspeed 4")).data();
 				ExecuteCommand(0, string1);
 				ExecuteCommand(0, string2);
-				ExecuteCommand(0, string3);
+				ExecuteCommand(0, string3);*/
 			}
 		}
         Sleep(100);
